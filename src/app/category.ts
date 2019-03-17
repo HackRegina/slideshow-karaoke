4e316e287d3e8ed7c8b1384b98aed44c08aa85a3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-17 01:12:32 +0000

    Deploy HackRegina/slideshow-karoke to github.com/HackRegina/slideshow-karoke.git:gh-pages

--- src/app/category.ts
+++
@@ -1,6 +0,0 @@
-export class Category {
-  category: string;
-  icon: string;
-  title: string;
-  image?: string
-}
--- src/app/slide.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from './category';
-export class Slide {
-  title: string;
-  imageurl: string;
-  link: string;
-  category: Category;
-}
